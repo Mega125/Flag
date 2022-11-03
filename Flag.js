6bb8cf685c1c81414fddaeb810dac5c40918c38f
Mega125 <113532121+Mega125@users.noreply.github.com>
2022-11-03 11:27:15 -0300

    Initial commit

---
+++ Flag.js
@@ -0,0 +1,40 @@
+var canvas = document.getElementById("sweden");
+var context = canvas.getContext("2d");
+context.beginPath();
+
+context.fillStyle = '#fdd835';
+context.fillRect(200, 0, 100, 800);
+// x, y, width, height
+
+context.fillStyle = '#fdd835';
+context.fillRect(0, 300, 800, 100);
+
+//
+
+var canvas = document.getElementById("italy");
+var context = canvas.getContext("2d");
+context.beginPath();
+
+context.fillStyle = '#008000';
+context.fillRect(0, 0, 270, 800);
+
+context.fillStyle = '#FF0000';
+context.fillRect(530, 0, 530, 800);
+
+//
+
+var canvas = document.getElementById("norway");
+var context = canvas.getContext("2d");
+context.beginPath();
+
+context.fillStyle = '#FFFFFF';
+context.fillRect(200, 0, 150, 800);
+
+context.fillStyle = '#FFFFFF';
+context.fillRect(0, 250, 800, 150);
+
+context.fillStyle = '#28387F';
+context.fillRect(230, 0, 90, 800);
+
+context.fillStyle = '#28387F';
+context.fillRect(0, 280, 800, 90);
